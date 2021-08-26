796e65638ad052704688f25ba4134c147542fcae
Barman Hrishikesh <hrishikesh.barman@volvo.com>
2021-08-26 21:23:04 +0530

    logic done

--- app.js
+++ app.js
@@ -3,34 +3,44 @@ const cashGiven = document.querySelector("#cash-given");
 const checkButton = document.querySelector("#check-button");
 const message = document.querySelector("#error-message");
 const noOfNotes = document.querySelectorAll(".no-of-notes");
+const nextStepButton = document.querySelector("#btn-next-step");
+const cashGivenLabel = document.querySelector(".cash-given-label");
 
+hiddenfnc()
 
 const availableNotes = [2000, 500, 100, 50, 10, 5, 1];
+
+nextStepButton.addEventListener('click', function shownextstep(){
+    
+    var billType = Number(billAmount.value)
+    if (billAmount.value == billType){
+        checkButton.style.display = "block";
+        cashGiven.style.display = "block";
+        cashGivenLabel.style.display = "block";
+    }else{
+        showMessage("Invalid input provided. Please enter a number");
+    }
+ 
+})
 
 
 checkButton.addEventListener("click", function validate() {
     
     hidemessage();//So that we don't always need to refresh page to enter a new amount
-
-    var billType = Number(billAmount.value)
-    if (billAmount.value == billType){
-        if (billAmount.value > 0) {
-            if (cashGiven.value >= billAmount.value) {
-                const amountToBeReturned = cashGiven.value - billAmount.value;
-                calculateChange(amountToBeReturned);
-            }
-            else{
-                showMessage("Insufficient cash provided")
-            }
+    if (billAmount.value > 0) {
+        if (cashGiven.value >= billAmount.value) {
+            const amountToBeReturned = cashGiven.value - billAmount.value;
+            calculateChange(amountToBeReturned);
         }
-        
         else{
-            showMessage("Bill amount should be greater than 0");
+            showMessage("Insufficient cash provided")
         }
-    }else{
-        showMessage("Invalid input provided. Please enter a number");
     }
- 
+    
+    else{
+        showMessage("Bill amount should be greater than 0");
+    }
+
     
 })
 
@@ -54,7 +64,12 @@ function showMessage(msg){
 }
 
 
+function hiddenfnc(){
+    checkButton.style.display = "none";
+    cashGiven.style.display = "none";
+    cashGivenLabel.style.display = "none";
 
+}
 
 
 
